5cb5d87b21602296b4b987d0ad3fecf9b3fe0cc0
Dashboardplayer <maarten.alexander83@email.com>
2025-03-31 00:43:15 +0200

    Fix crypto polyfill to avoid setting protected window property

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,29 +1,19 @@
 import { Buffer } from 'buffer';
 import process from 'process';
-import cryptoPolyfill from './crypto-polyfill';
+import cryptoBrowserify from 'crypto-browserify';
+import stream from 'stream-browserify';
+import path from 'path-browserify';
+import os from 'os-browserify/browser';
 
-// Basic polyfills
-if (typeof window !== 'undefined') {
-  window.Buffer = window.Buffer || Buffer;
-  window.process = window.process || process;
+// Make non-protected globals available
+window.Buffer = Buffer;
+window.process = process;
+window.stream = stream;
+window.path = path;
+window.os = os;
 
-  // Handle crypto polyfill
-  if (window.crypto) {
-    Object.keys(cryptoPolyfill).forEach(key => {
-      if (typeof window.crypto[key] === 'undefined') {
-        try {
-          if (typeof cryptoPolyfill[key] === 'function') {
-            Object.defineProperty(window.crypto, key, {
-              enumerable: true,
-              configurable: true,
-              writable: true,
-              value: cryptoPolyfill[key].bind(cryptoPolyfill)
-            });
-          }
-        } catch (e) {
-          console.warn(`Could not add ${key} to window.crypto:`, e);
-        }
-      }
-    });
-  }
-} 
+// For crypto, we'll export it for direct usage rather than trying to set it on window
+const crypto = cryptoBrowserify;
+
+// Export all polyfills for direct import where needed
+export { Buffer, process, crypto, stream, path, os }; 
